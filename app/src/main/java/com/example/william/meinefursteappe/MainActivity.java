87aa4ad9dcbc9aa2de40121a16ee9d89e05e15ec
William T <william.tam18@gmail.com>
2017-08-28 13:30:25 +0800

    something

--- app/src/main/java/com/example/william/meinefursteappe/MainActivity.java
+++ app/src/main/java/com/example/william/meinefursteappe/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.william.meinefursteappe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// The main activity (the entry point of the app.
+// When building and running the app, the system launches an instance of this
+// Activity and loads its layout
 public class MainActivity extends AppCompatActivity {
 
     @Override
