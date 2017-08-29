ba84e2422ee16985d5c38f2020222913204d34bc
William T <william.tam18@gmail.com>
2017-08-29 14:12:54 +0800

    Maintenant il y a un activité deuxieme complete, apres vous tappez la bouton

--- app/src/main/java/com/example/william/meinefursteappe/DisplayMessageActivity.java
+++ app/src/main/java/com/example/william/meinefursteappe/DisplayMessageActivity.java
@@ -1,7 +1,9 @@
 package com.example.william.meinefursteappe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -10,9 +12,19 @@ public class DisplayMessageActivity extends AppCompatActivity {
     // Creates corresponding activity_display_message.xml layout file
     // Adds the required <activity> element in AndroidManifest.xml
 
+    // Display the message passed by the first activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        // once again use Option+Return to import missing classes
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
